face1483e1656e5ce1d572a2b7e4602ca8374bfd
Ari Toivanen <ari.toivanen@edu.karelia.fi>
2023-08-16 22:32:09 +0300

    Tehtävän 3.2 jälkeinen tilanne

--- index.js
+++ index.js
@@ -2,34 +2,38 @@ const express = require('express')
 const app = express()
 
 let persons = [
+
     {
-        "persons": [
-            {
-                "name": "Arto Hellas",
-                "number": "040-123456",
-                "id": 1
-            },
-            {
-                "name": "Ada Lovelace",
-                "number": "39-44-5323523",
-                "id": 2
-            },
-            {
-                "name": "Dan Abramov",
-                "number": "12-43-234345",
-                "id": 3
-            },
-            {
-                "name": "Mary Poppendieck",
-                "number": "39-23-6423122",
-                "id": 4
-            }
-        ]
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
     }
+
 ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello Puhelinluettelo!</h1>')
+})
+
+app.get('/info', (req, res) => {
+    const string = `<div>Phonebook has info for ${persons.length} persons</div>
+    <div>${new Date()}</div>`
+    res.send(string)
 })
 
 app.get('/api/persons', (req, res) => {
