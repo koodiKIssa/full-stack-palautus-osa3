c926e5a884ac6f0f586536b8d2f62b94887cb144
Ari Toivanen <ari.toivanen@edu.karelia.fi>
2023-08-20 23:41:53 +0300

    Tehtävän 3.9 jälkeinen tilanne

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
-//app.use(morgan(:method :url :status :res[content-length] - :response-time ms))
+app.use(express.static('build'))
 
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
