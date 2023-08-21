1b4088278148f08dee00337e227419aabf8457d4
Ari Toivanen <ari.toivanen@edu.karelia.fi>
2023-08-21 21:55:35 +0300

    Tehtävän 3.18 jälkeinen tilanne

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.static('build'))
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
+
 /* let persons = [
 
     {
@@ -37,48 +39,63 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 ] */
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello Puhelinluettelo!</h1>')
-})
-
 app.get('/info', (req, res) => {
-    const string = `<div>Phonebook has info for ${persons.length} persons</div>
-    <div>${new Date()}</div>`
-    res.send(string)
+
+    Person.find({})
+        .then(persons => {
+            res.send(
+                `<div>Phonebook has info for ${persons.length} persons</div>
+        <div>${new Date()}</div>`
+            )
+        })
+        .catch(error => next(error))
+
+    /*  const string = `<div>Phonebook has info for 5 persons</div>
+     <div>${new Date()}</div>`
+     res.send(string) */
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     //res.json(persons)
 
-    Person.find({}).then(persons => {
-        res.json(persons)
-    })
+    Person.find({})
+        .then(persons => {
+            res.json(persons)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
+    /* const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
 
     if (person) {
         res.json(person)
     } else {
         res.status(404).end()
-    }
+    } */
+
+    Person.findById(req.params.id)
+        .then(result => {
+            res.json(result)
+        })
+        .catch(error => next(error))
+
 })
 
-app.delete('/api/persons/:id', (req, res) => {/* 
+app.delete('/api/persons/:id', (req, res, next) => {/* 
     const id = Number(req.params.id)
     persons = persons.filter(p => p.id !== id)
     res.status(204).end() */
 
     Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -101,11 +118,37 @@ app.post('/api/persons', (req, res) => {
 
     //persons = persons.concat(person)
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
