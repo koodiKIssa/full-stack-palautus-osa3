62114a20cbbc93b30c97f7185d7b03aa06d78919
Ari Toivanen <ari.toivanen@edu.karelia.fi>
2023-08-21 23:44:41 +0300

    Tehtävän 3.21 jälkeinen tilanne

--- models/person.js
+++ models/person.js
@@ -17,8 +17,20 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}\b-\d{1,}$/.test(v);
+            },
+        }
+    }
 })
 
 personSchema.set('toJSON', {
